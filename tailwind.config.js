a98b3f0004fde65ed7db8542ed4b1eaa0548bb22
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-04 09:42:26 +0200

    setup tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        13: '3.25rem',
+        15: '3.75rem',
+        120: '29rem',
+        128: '32rem',
+        144: '36rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
